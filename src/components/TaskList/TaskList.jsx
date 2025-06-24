5c4ef9022d9223a3abf36f271ceaed0866f2796d
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-24 16:27:48 +0530

    added scrolable tasklist

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,13 +2,92 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='bg-red-500 h-[55%] py-10 mt-10 flex items-center justify-start gap-5 overflow-x-auto'>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
-        <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400'></div>
+    <div id='tasklist' className=' h-[55%] py-10 mt-10 flex items-center justify-start gap-5 overflow-x-auto rounded-xl'>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-red-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-green-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-blue-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-cyan-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-yellow-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-red-400 p-5'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    High
+                </h3>
+                <h4 className='text-sm text-white'>
+                    24 june 2025
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold text-white'>Make a react project</h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                Reprehenderit nisi incididunt deserunt excepteur ipsum tempor nostrud dolor fugiat consectetur ex esse. Commodo sit in magna consequat voluptate. Labore amet aliqua in pariatur ea veniam proident consectetur non sint occaecat commodo nulla fugiat.
+            </p>
+        </div>
+       
     </div>
   )
 }
