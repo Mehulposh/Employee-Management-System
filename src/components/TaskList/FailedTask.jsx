2301d197bf4dc14beb7f9c674cf5d59f99ccb979
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-24 23:47:34 +0530

    created different cards for different task status

---
+++ src/components/TaskList/FailedTask.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const FailedTask = ({task}) => {
+   return (
+        <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-red-400 p-5' key={task.id}>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
+                    {task.priority}
+                </h3>
+                <h4 className='text-sm text-white'>
+                    {task.date}
+                </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-bold text-white'>
+                {task.title}
+            </h2>
+            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
+                {task.description}
+            </p>
+            <div className=' mt-4 '>
+                <button className='bg-red-500 w-full py-1 px-2 text-sm'> Failed</button>
+            </div>
+        </div>
+  )
+}
+
+export default FailedTask
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
+import AcceptedTask from './AcceptedTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
+import NewTask from './NewTask'
+
 
 const TaskList = ({data}) => {
    
   return (
     <div id='tasklist' className=' h-[55%] py-10 mt-10 flex items-center justify-start gap-5 overflow-x-auto rounded-xl'>
         
-        {data.map(task => (
-            <div className='h-full rounded-xl flex-shrink-0 w-[300px] bg-red-400 p-5' key={task.id}>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-700 px-3 py-1 rounded text-sm text-white'>    
-                    {task.priority}
-                </h3>
-                <h4 className='text-sm text-white'>
-                    {task.date}
-                </h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-bold text-white'>
-                {task.title}
-            </h2>
-            <p className='text-sm font-medium text-gray-200 text-justify mt-2'>
-                {task.description}
-            </p>
-        </div>
-        ))}
+        {data.map(task => {
+            if(task.active){
+                return <AcceptedTask task={task}/>
+            }
+            if(task.newTask){
+                return <NewTask task={task} />
+            }
+        })}
        
     </div>
   )
