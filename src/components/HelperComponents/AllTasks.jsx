c905ceda8f464d5243e2c126da5b15e059293e96
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-06-25 10:46:05 +0530

    added employee task count in admin dashboard

--- src/components/HelperComponents/AllTasks.jsx
+++ src/components/HelperComponents/AllTasks.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTasks = () => {
+    const authData = useContext(AuthContext);
   return (
-    <div id='alltasks' className='bg-[#1C1C1C] rounded p-5 mt-5 h-40 overflow-auto'>
-        <div className='bg-red-400 mb-2 py-2 px-4 rounded flex justify-between'>
-            <h2>Mehul</h2>
-            <h3>Create a Nav bar</h3>
-            <h5>Status</h5>
+    <div  className='bg-[#1C1C1C] rounded p-5 mt-5 h-50 '>
+        <div className='bg-red-400 mb-2 py-2 px-4 rounded flex justify-between items-center'>
+            <h2 className='w-1/5 text-xl'>Employee Name</h2>
+            <h3 className='w-1/5 text-xl'>New Task</h3>
+            <h5 className='w-1/5 text-xl'>Accepted Task</h5>
+            <h5 className='w-1/5 text-xl'>Complete Task</h5>
+            <h5 className='w-1/5 text-xl'>Failed Task</h5>
         </div>
-        <div className='bg-cyan-400 mb-2 py-2 px-4 rounded flex justify-between'>
-            <h2>Mehul</h2>
-            <h3>Create a Nav bar</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-green-400 mb-2 py-2 px-4 rounded flex justify-between'>
-            <h2>Mehul</h2>
-            <h3>Create a Nav bar</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-yellow-400 mb-2 py-2 px-4 rounded flex justify-between'>
-            <h2>Mehul</h2>
-            <h3>Create a Nav bar</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-emerald-400 mb-2 py-2 px-4 rounded flex justify-between'>
-            <h2>Mehul</h2>
-            <h3>Create a Nav bar</h3>
-            <h5>Status</h5>
-        </div>
+       <div id='alltasks' className='overflow-auto h-[80%]'>
+            {authData.employeesData.map(data => (
+                <div className='border border-red-400 mb-2 py-2 px-4 rounded flex justify-between items-center' key={data.id}>
+                    <h2 className='w-1/5 text-white text-xl '>{data.name}</h2>
+                    <h3 className='w-1/5 text-blue-500 text-xl font-bold'>{data.taskCount.newTask}</h3>
+                    <h5 className='w-1/5 text-purple-400 text-xl font-bold'>{data.taskCount.active}</h5>
+                    <h5 className='w-1/5 text-green-300 text-xl font-bold'>{data.taskCount.completed}</h5>
+                    <h5 className='w-1/5 text-indigo-500 text-xl font-bold'>{data.taskCount.failed}</h5>
+                </div>
+            ))}
+       </div>
+       
     </div>
   )
 }
