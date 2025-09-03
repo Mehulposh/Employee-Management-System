ac7e9c7d12976dd5b64958c412c2c540000697bc
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-03 19:10:50 +0530

    connected mongo database

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,32 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const UserRoutes = require('./routes/UserRoutes');
+const { default: mongoose } = require('mongoose');
+
+//startinf port 
 const PORT = process.env.PORT
 
+//server created
 const server = express();
 
 server.use(cors());
 
+//universal middleware
 server.use(express.json());
 
-server.use('/api/v1',UserRoutes)
+server.use('/api/v1/user',UserRoutes)
+
+
+const dbName = process.env.DB_NAME;
+const dbPort = process.env.DB_PORT;
+const dbHost = process.env.DB_HOST;
+// database connection 
+mongoose.connect(`${dbHost}:${dbPort}/${dbName}`)
+.then(() => {
+    console.log("thumbs up DB IS CONNECTED")
+})
+
+
 server.listen(PORT, () => {
     console.log("THumbs up every time is good port listning is active on expressJS",PORT)
 })
